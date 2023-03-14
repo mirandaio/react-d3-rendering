0299a173f9c5dd292659f72d1ee52f8c6bf1639d
Alfredo Miranda <alfredo.miranda@signal-ai.com>
2023-03-14 09:27:16 +0000

    use Link component

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,11 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Root() {
   return (
     <>
       <h1>React + D3 rendering</h1>
       <main>
         <ul>
-          <li><a href="/react-rendering">React rendering</a></li>
-          <li><a href="/d3-rendering">D3 rendering</a></li>
+          <li>
+            <Link to="/react-rendering">React rendering</Link>
+          </li>
+          <li>
+            <Link to="/d3-rendering">D3 rendering</Link>
+          </li>
         </ul>
       </main>
     </>
