47bf6d047f8f7ee48891290c2fce2decd19183e7
Alfredo Miranda <alfredo.miranda@signal-ai.com>
2023-03-18 14:23:00 +0000

    tweak scales

--- src/components/ReactRendering.jsx
+++ src/components/ReactRendering.jsx
@@ -12,29 +12,27 @@ import {
 const xScale = d3
   .scaleLinear()
   .domain([0, MAX_X])
-  .range([0, WIDTH - 2 * PADDING]);
+  .range([PADDING, WIDTH - PADDING]);
 
 const yScale = d3
   .scaleLinear()
   .domain([0, MAX_Y])
-  .range([HEIGHT - 2 * PADDING, 0]);
+  .range([HEIGHT - PADDING, PADDING]);
 
 export default function ReactRendering() {
   return (
     <svg width={WIDTH} height={HEIGHT} style={{ outline: "1px solid red" }}>
-      <g transform={`translate(${PADDING}, ${PADDING})`}>
-        {data.map((d, i) => {
-          return (
-            <path
-              d={pathData[d.shape]}
-              key={i}
-              fill={d.color}
-              stroke="#999"
-              transform={`translate(${xScale(d.x)}, ${yScale(d.y)})`}
-            />
-          );
-        })}
-      </g>
+      {data.map((d, i) => {
+        return (
+          <path
+            d={pathData[d.shape]}
+            key={i}
+            fill={d.color}
+            stroke="#999"
+            transform={`translate(${xScale(d.x)}, ${yScale(d.y)})`}
+          />
+        );
+      })}
     </svg>
   );
 }
