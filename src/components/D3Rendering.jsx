11d2026c50ba65fdd5c7dc231385a2a0b8d28b18
Alfredo Miranda <alfredo@miranda.io>
2023-03-18 17:40:43 +0000

    use d3 for rendering

--- src/components/D3Rendering.jsx
+++ src/components/D3Rendering.jsx
@@ -1,5 +1,5 @@
 import * as d3 from "d3";
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 
 import {
   data,
@@ -11,12 +11,36 @@ import {
   MAX_Y,
 } from "../generate-data";
 
+const xScale = d3
+  .scaleLinear()
+  .domain([0, MAX_X])
+  .range([PADDING, WIDTH - PADDING]);
+
+const yScale = d3
+  .scaleLinear()
+  .domain([0, MAX_Y])
+  .range([HEIGHT - PADDING, PADDING]);
+
 export default function D3Rendering() {
+  const svgRef = useRef(null);
+
+  useEffect(() => {
+    d3.select(svgRef.current)
+      .selectAll("path")
+      .data(data)
+      .join("path")
+      .attr("d", (d) => pathData[d.shape])
+      .attr("fill", (d) => d.color)
+      .attr("stroke", "#999")
+      .attr("transform", (d) => `translate(${xScale(d.x)}, ${yScale(d.y)})`);
+  }, []);
+
   return (
     <svg
-      with={WIDTH}
+      ref={svgRef}
+      width={WIDTH}
       height={HEIGHT}
       style={{ outline: "1px solid red" }}
-    ></svg>
+    />
   );
 }
