9ca9dc77b030b33674dca0594e2e5c8647e6664e
Alfredo Miranda <alfredo@miranda.io>
2023-03-18 19:16:52 +0000

    update styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from './routes/root';
 import ReactRenderingPage from './routes/react-rendering-page';
 import D3RenderingPage from './routes/d3-rendering-page';
-// import App from './App'
 import './index.css';
 
 const router = createBrowserRouter([
