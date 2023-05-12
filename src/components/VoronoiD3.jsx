46ddd474b17efc1ccec9d73f288164c11c94ac5b
Alfredo Miranda <alfredo@miranda.io>
2023-05-12 14:59:36 +0100

    render voronoi with d3

---
+++ src/components/VoronoiD3.jsx
@@ -0,0 +1,38 @@
+import { useRef, useEffect } from 'react';
+import { useOutletContext } from 'react-router-dom';
+import * as d3 from 'd3';
+import { WIDTH, HEIGHT, PADDING, MAX_X, MAX_Y } from '../generate-data';
+
+const xScale = d3
+  .scaleLinear()
+  .domain([0, MAX_X])
+  .range([PADDING, WIDTH - PADDING]);
+
+const yScale = d3
+  .scaleLinear()
+  .domain([0, MAX_Y])
+  .range([HEIGHT - PADDING, PADDING]);
+
+export default function VoronoiD3() {
+  const data = useOutletContext();
+  const svgRef = useRef(null);
+
+  useEffect(() => {
+    const delaunay = d3.Delaunay.from(
+      data,
+      (d) => xScale(d.x),
+      (d) => yScale(d.y)
+    );
+    const voronoi = delaunay.voronoi([0, 0, WIDTH, HEIGHT]);
+
+    d3.select(svgRef.current)
+      .selectAll('path')
+      .data(data)
+      .join('path')
+      .attr('d', (d, i) => voronoi.renderCell(i))
+      .attr('fill', (d) => d.color)
+      .attr('stroke', '#aaa');
+  }, [data]);
+
+  return <svg ref={svgRef} width={WIDTH} height={HEIGHT} />;
+}
--- src/routes/d3-rendering-page.jsx
+++ src/routes/d3-rendering-page.jsx
@@ -1,10 +1,11 @@
 import D3Rendering from '../components/D3Rendering';
+import VoronoiD3 from '../components/VoronoiD3';
 
 export default function D3RenderingPage() {
   return (
     <>
       <h2>D3 rendering</h2>
-      <D3Rendering />
+      <VoronoiD3 />
     </>
   );
 }
