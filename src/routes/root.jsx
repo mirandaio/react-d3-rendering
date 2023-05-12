de3480973adbe9edc2e14010e1d52a3f401758e4
Alfredo Miranda <alfredo@miranda.io>
2023-05-12 18:55:44 +0100

    add lag radar

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import LagRadar from 'react-lag-radar';
 import { Link, Outlet } from 'react-router-dom';
 import { generateData } from '../generate-data';
 
@@ -13,20 +14,25 @@ export default function Root() {
     <>
       <h1>React + D3 rendering</h1>
       <main>
-        <ul>
-          <li>
-            <Link to="/react-rendering">React rendering</Link>
-          </li>
-          <li>
-            <Link to="/d3-rendering">D3 rendering</Link>
-          </li>
-          <li>
-            <Link to="/canvas-rendering">Canvas rendering</Link>
-          </li>
-        </ul>
-        <input type="range" min="10" max="1000" onChange={handleOnChange} />
         <section>
-          <Outlet context={data} />
+          <LagRadar size="300" />
+        </section>
+        <section className="showcase">
+          <ul>
+            <li>
+              <Link to="/react-rendering">React rendering</Link>
+            </li>
+            <li>
+              <Link to="/d3-rendering">D3 rendering</Link>
+            </li>
+            <li>
+              <Link to="/canvas-rendering">Canvas rendering</Link>
+            </li>
+          </ul>
+          <input type="range" min="10" max="10000" onChange={handleOnChange} />
+          <section>
+            <Outlet context={data} />
+          </section>
         </section>
       </main>
     </>
