43fd709ad1ae52fbf41f785caf206f7d8f282a31
Alfredo Miranda <alfredo@miranda.io>
2023-05-13 15:37:58 +0100

    update

--- src/components/VoronoiD3.jsx
+++ src/components/VoronoiD3.jsx
@@ -13,34 +13,59 @@ const yScale = d3
   .domain([0, MAX_Y])
   .range([HEIGHT - PADDING, PADDING]);
 
+const render = (data, svg) => {
+  const delaunay = d3.Delaunay.from(
+    data,
+    (d) => xScale(d.x),
+    (d) => yScale(d.y)
+  );
+  const voronoi = delaunay.voronoi([0, 0, WIDTH, HEIGHT]);
+
+  d3.select(svg)
+    .selectAll('path')
+    .data(data)
+    .join('path')
+    .attr('d', (d, i) => voronoi.renderCell(i))
+    .attr('fill', (d) => d.color)
+    .attr('stroke', '#aaa');
+
+  d3.select(svg)
+    .selectAll('circle')
+    .data(data)
+    .join('circle')
+    .attr('r', 2)
+    .attr('cx', (d) => xScale(d.x))
+    .attr('cy', (d) => yScale(d.y));
+};
+
 export default function VoronoiD3() {
-  const data = useOutletContext();
+  const [data, setData] = useOutletContext();
   const svgRef = useRef(null);
 
   useEffect(() => {
-    const delaunay = d3.Delaunay.from(
-      data,
-      (d) => xScale(d.x),
-      (d) => yScale(d.y)
-    );
-    const voronoi = delaunay.voronoi([0, 0, WIDTH, HEIGHT]);
-
-    d3.select(svgRef.current)
-      .selectAll('path')
-      .data(data)
-      .join('path')
-      .attr('d', (d, i) => voronoi.renderCell(i))
-      .attr('fill', (d) => d.color)
-      .attr('stroke', '#aaa');
-
-    d3.select(svgRef.current)
-      .selectAll('circle')
-      .data(data)
-      .join('circle')
-      .attr('r', 2)
-      .attr('cx', (d) => xScale(d.x))
-      .attr('cy', (d) => yScale(d.y));
+    render(data, svgRef.current);
   }, [data]);
 
-  return <svg ref={svgRef} width={WIDTH} height={HEIGHT} />;
+  const handleOnMouseMove = (event) => {
+    const svg = svgRef.current;
+    const rect = svg.getBoundingClientRect();
+
+    const newData = [...data];
+    newData[0] = {
+      ...newData[0],
+      x: xScale.invert(event.clientX - rect.left),
+      y: yScale.invert(event.clientY - rect.top),
+    };
+
+    setData(newData);
+  };
+
+  return (
+    <svg
+      ref={svgRef}
+      width={WIDTH}
+      height={HEIGHT}
+      onMouseMove={handleOnMouseMove}
+    />
+  );
 }
